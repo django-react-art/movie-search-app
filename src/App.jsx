dbd1f493801158a7fbe9544f347ec7c34eb32c2e
Donny <bajracharyanimesh2022@gmail.com>
2026-03-29 12:59:49 -0500

    Fix movie search: display errors correctly and clear movies on failure

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,45 @@ import { useState, useEffect } from "react";
 
 function App(){
   const [movies, setMovies] = useState([]);
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
 
   const fetchMovies = async(query) => {
+    if(!query){
+      setError("Search Empty!");
+      setMovies([]);
+      return;
+    }
     try{
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=18ef985b&s=${query}`
     )
     const data = await response.json();
+    if (data.Response === "False") {
+      setError(data.Error);
+    }
+    else{
     console.log(data);
     setMovies(data.Search || []);
-    }
+    setError("");
+    }}
     catch(error){
       console.error(error);
     }
   }
   useEffect(() => {
-    fetchMovies("batman");
-  }, []);
+    fetchMovies(input);
+  }, [input]);
 
   return(
     <div>
-      {movies.map((movie) => (
+      <input type="text" placeholder="Enter Movie Name Here" value = {input} onChange={(e)=> setInput(e.target.value)} />
+      {error ? (
+        <p>{error}</p>
+      ) : ( 
+      movies.map((movie) => (
         <p key = {movie.imdbID}>{movie.Title}</p>
-      ))}
+      )))}
     </div>
   )
 }
