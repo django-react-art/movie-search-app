91267fe1bcfa2cb8edb8afb998742faf72f6ee13
Donny <bajracharyanimesh2022@gmail.com>
2026-04-03 12:48:02 -0500

    UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
 
 function App(){
   const [movies, setMovies] = useState([]);
@@ -35,13 +36,41 @@ function App(){
   return(
     <div>
       <input type="text" placeholder="Enter Movie Name Here" value = {input} onChange={(e)=> setInput(e.target.value)} />
+    <Grid container spacing = {3}>
       {error ? (
         <p>{error}</p>
       ) : ( 
       movies.map((movie) => (
-        <p key = {movie.imdbID}>{movie.Title}</p>
+        <Grid item xs = {12} sm = {6} md = {4} key = {movie.imdbID}>
+          <Card
+          sx = {{
+            cursor: "pointer", 
+            transition: "0.3s",
+            "&:hover": {
+              transform: "scale(1.05)",
+              boxShadow: 6,
+            },
+          }}>
+  <CardMedia
+    component="img"
+    height="300"
+    image={
+      movie.Poster !== "N/A"
+        ? movie.Poster
+        : "https://via.placeholder.com/150"
+    }
+    alt={movie.Title}
+  />
+
+  <CardContent>
+    <Typography variant="h6">
+      {movie.Title}
+    </Typography>
+  </CardContent>
+</Card></Grid>
       )))}
-    </div>
+      </Grid>
+  </div>
   )
 }
 
